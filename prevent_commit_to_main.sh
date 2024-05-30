e2ec57a102c55b5a36b46eabf579aa469ba1208e
= <Sparks29032@gmail.com>
2024-05-30 17:24:13 -0400

    Allow merge to main

--- prevent_commit_to_main.sh
+++ prevent_commit_to_main.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env sh
 ##!"C:\\Program Files\\Git\\usr\\bin\\bash.exe"
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
 if [ "$branch" = "main" ]; then
-echo "You are on the main branch. Committing to the main branch is not allowed."
-exit 1
+	if [ -e "${GIT_DIR}/MERGE_MODE" ]; then
+		echo "Merge to main allowed."
+		exit 0
+	else
+		echo "You are on the main branch. Committing to the main branch is not allowed."
+		exit 1
+	fi
 fi
