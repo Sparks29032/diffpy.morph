1d7c092c4726e9df7b7a66b39a3ca42460d450cb
= <Sparks29032@gmail.com>
2024-05-30 17:37:23 -0400

    Remove exit 0

--- prevent_commit_to_main.sh
+++ prevent_commit_to_main.sh
@@ -6,7 +6,6 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$branch" = "main" ]; then
 	if [ -e "${GIT_DIR}/MERGE_MODE" ]; then
 		echo "Merge to main allowed."
-		exit 0
 	else
 		echo "You are on the main branch. Committing to the main branch is not allowed."
 		exit 1
