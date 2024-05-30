d73843f7399c038a207c95e5035408da1ab6e5da
= <Sparks29032@gmail.com>
2024-05-30 17:14:22 -0400

    git fix

--- prevent_commit_to_main.sh
+++ prevent_commit_to_main.sh
@@ -3,7 +3,7 @@
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
 
-if [ "$branch" = "main" ] && [ -z "$MERGE_HEAD" ]; then
+if [ "$branch" = "main" ] && [ -z "${GIT_DIR}/MERGE_MODE" ]; then
 echo "You are on the main branch. Committing to the main branch is not allowed."
 exit 1
 fi
