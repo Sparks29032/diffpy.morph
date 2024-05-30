f9b011e3310a3b8a4634cf1fe9d6f6c08b157490
= <Sparks29032@gmail.com>
2024-05-30 18:13:53 -0400

    testign

--- prevent_commit_to_main.sh
+++ prevent_commit_to_main.sh
@@ -4,10 +4,7 @@
 branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$branch" = "main" ]; then
-	echo $GIT_DIR
-	echo $GIT_DIR/MERGE_HEAD
-	echo $GIT_DIR/MERGE_MODE
-	if [ -f $GIT_DIR/MERGE_HEAD ]; then
+	if [ "$MERGE" == "true" ]; then
 		echo "Merge to main allowed."
 	else
 		echo "You are on the main branch. Committing to the main branch is not allowed."
